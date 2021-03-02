ed2370408ee515d65649fd6a70e17a7df5275d6a
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2021-03-02 16:00:43 -0300

    add primeira query para busca de horário

---
+++ schema-query/projeto/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Query {
+        horaAtual: String
+    }
+
+`
+const resolvers = {
+    Query: {
+        horaAtual() {
+            return `${new Date()}`;
+        }
+
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+});
