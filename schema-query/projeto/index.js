cc7b13c859f54de90b412b8d3cda7a666d911feb
Evandro Barbosa dos Reis <evandrobarbosadosreis@gmail.com>
2021-03-02 16:12:24 -0300

    add query hello world e tipo complexo

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,15 +1,45 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
+    
+    scalar Date
+
+    type Usuario {
+        id: ID
+        nome: String
+        email: String
+        idade: Int,
+        salario: Float
+        ativo: Boolean
+    }
+
     type Query {
-        horaAtual: String
+        olaMundo: String
+        horaAtual: Date
+        usuarioLogado: Usuario
     }
 
 `
 const resolvers = {
     Query: {
+
+        olaMundo() {
+            return 'Olá, mundo!'
+        },
+
         horaAtual() {
-            return `${new Date()}`;
+            return new Date()
+        },
+
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Ana da Web',
+                email: 'anaweb@email.com',
+                idade: 25,
+                salario: 1500.25,
+                ativo: true
+            }
         }
 
     }
